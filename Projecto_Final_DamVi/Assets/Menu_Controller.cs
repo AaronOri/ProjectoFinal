48c754bb4ac491e796ba72227a95ea3a66b00506
253901133803\Argo <a23migcergal@inspedralbes.cat>
2025-05-06 16:32:12 +0200

    Menú principal creado con sus funciones corresponientes

--- Projecto_Final_DamVi/Assets/Menu_Controller.cs
+++ Projecto_Final_DamVi/Assets/Menu_Controller.cs
@@ -1,18 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Menu_Controller : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+   
+    public void Jugar()
     {
-        
+        SceneManager.LoadScene("Level1");
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Inf()
     {
-        
+        SceneManager.LoadScene("NivelInfinito");
     }
+
+
+    public void Salir()
+    {
+           Application.Quit();
+    }
+
 }
